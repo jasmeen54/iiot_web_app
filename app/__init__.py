d4e41ad87ee0cc4c5870549cf00bfe5a6282f8b5
jasmeen54 <emeenafandi@gmail.com>
2024-04-25 16:00:41 +0200

    update __initi__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,26 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 from app.fetch_data import list_blob_data
 from app.process_blob_data import process_blob_data
 import json
 
 app = Flask(__name__)
 
+# Load configuration from config.json
+#def load_config():
+    #with open('config.json', 'r') as f:
+        #config = json.load(f)
+    #return config
+
 # Define route to list blob data
 @app.route('/')
 def list_blobs():
     # Load configuration
+    #config = load_config()
+    #blob_connection_string = config.get('BLOB_CONNECTION_STRING')
+    #container_name = config.get('CONTAINER_NAME')
     blob_connection_string = ${{ secrets.AZURE_BLOB_STORAGE_CONNECTION_STRING }}
-    container_name = 'dataiiot'
+    container_name = "dataiiot"
 
-    
     # Retrieve blob data
     blob_data_list = list_blob_data(blob_connection_string, container_name)
 
