38102c39129843b6d943100879403a2aa6a9a17d
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-04-26 10:37:44 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,8 @@ from app.fetch_data import list_blob_data
 from app.process_blob_data import process_blob_data
 import json
 import os
-import logging
-
-# Set logging configuration
-logging.basicConfig(level=logging.DEBUG)  # Set logging level to DEBUG
 
 app = Flask(__name__)
-
-# Define a logger
-logger = logging.getLogger(__name__)
 
 # Load configuration from config.json
 #def load_config():
@@ -22,23 +15,11 @@ logger = logging.getLogger(__name__)
 # Define route to list blob data
 @app.route('/')
 def list_blobs():
-    # Load configuration
-    #config = load_config()
-    #blob_connection_string = config.get('BLOB_CONNECTION_STRING')
-    #container_name = config.get('CONTAINER_NAME')
-    
     blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
     container_name = os.environ.get('AZURE_CONTAINER_NAME')
 
-    # Log the connection string and container name
-    logger.debug(f'Blob Connection String: {blob_connection_string}')
-    logger.debug(f'Container Name: {container_name}')
-
     # Retrieve blob data
     blob_data_list = list_blob_data(blob_connection_string, container_name)
-
-    # Log the number of blobs retrieved
-    logger.debug(f'Number of Blobs Retrieved: {len(blob_data_list)}')
 
     # Process blob data and organize it into sensor_data dictionary
     sensor_data = process_blob_data(blob_data_list)
