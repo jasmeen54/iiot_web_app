10e42b9fffe9f4ee991032f102253ec73f2c6285
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-02 14:05:56 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-from app.fetch_data import list_blob_data
-from app.process_blob_data import process_blob_data
+from app.fetch_data import list_blobs_in_container
 import json
 import os
 
@@ -12,20 +11,16 @@ def list_blobs():
     blob_connection_string = os.environ.get['AZURE_BLOB_STORAGE_CONNECTION_STRING'] 
     container_name = os.environ.get['AZURE_CONTAINER_NAME']
 
-    # Retrieve blob data
-    blob_data_list = list_blob_data(blob_connection_string, container_name)
-
-    # Process blob data and organize it into sensor_data dictionary
-    sensor_data = process_blob_data(blob_data_list)
+    # List blobs in the container
+    blobs = list_blobs_in_container(blob_connection_string, container_name)
     
-    if sensor_data:
-        return render_template("index.html", sensor_data=sensor_data)
+    if blobs:
+        return render_template("index.html", blobs=blobs)
     else:
-        return "Error occurred while listing blob data."
+        return "Error occurred while listing blobs."
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
 
 
