8a62cd1f308df30c70bcf7aa2a98f8df877e6951
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-02 15:39:05 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template
 from app.fetch_data import list_blobs_in_container
 import json
-import os
 
 app = Flask(__name__)
 
-# Define route to list blob data
+# Load configuration from config.json
+def load_config():
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+# Define route to list blobs
 @app.route('/')
 def list_blobs():
-    blob_connection_string = os.environ.get['AZURE_BLOB_STORAGE_CONNECTION_STRING'] 
-    container_name = os.environ.get['AZURE_CONTAINER_NAME']
-
+    # Load configuration
+    config = load_config()
+    blob_connection_string = config.get('BLOB_CONNECTION_STRING')
+    container_name = config.get('CONTAINER_NAME')
+    
     # List blobs in the container
     blobs = list_blobs_in_container(blob_connection_string, container_name)
     
