1332fdfe4dd71995e227d4f5d3d05b61afd3af83
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-13 11:33:34 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,18 +8,21 @@ app = Flask(__name__)
 # Define route to list blobs
 @app.route('/')
 def list_blobs():
-    # Load configuration
-    config = load_config()
-    blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
-    container_name = "dataiiot"
-    
-    # List blobs in the container
-    blobs = list_blobs_in_container(blob_connection_string, container_name)
-    
-    if blobs:
-        return render_template("index.html", blobs=blobs)
-    else:
-        return "Error occurred while listing blobs."
+    try:
+        # Load configuration
+        config = load_config()
+        blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
+        container_name = "dataiiot"
+        
+        # List blobs in the container
+        blobs = list_blobs_in_container(blob_connection_string, container_name)
+        
+        if blobs:
+            return render_template("index.html", blobs=blobs)
+        else:
+            return "No blobs found in the container."
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 if __name__ == '__main__':
     app.run(debug=True)
