66ecac6dd33a81f03c4a61af2513cdc9d97549e5
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-15 11:14:26 +0200

    Update fetch_data.py

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -3,6 +3,9 @@ import os
 
 def list_blobs_in_container(connection_string, container_name):
     try:
+        if not connection_string:
+            raise ValueError("Connection string is None.")
+        
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         container_client = blob_service_client.get_container_client(container_name)
         
@@ -16,7 +19,7 @@ def list_blobs_in_container(connection_string, container_name):
         return blobs_in_folders
     
     except Exception as e:
-        print(f"Error retrieving blobs: {e}")
+        print(f"Error listing blob names: {e}")
         return None
 
 
