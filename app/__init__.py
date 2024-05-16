141689cbc93844276deb38cfcaee4c33ed5f8c54
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-16 11:50:58 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,43 +1,42 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, jsonify
 from app.fetch_data import list_and_fetch_blobs_in_container
 from app.process_blob_data import process_blob_data
-import json
 import os
+import json
+import threading
+import time
 
 app = Flask(__name__)
 
-# Define route to list and display blobs
+sensor_data = {}
+
+def update_data():
+    while True:
+        blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
+        container_name = os.environ.get('AZURE_CONTAINER_NAME')
+
+        # List and fetch blobs in the container
+        blobs_data = list_and_fetch_blobs_in_container(blob_connection_string, container_name)
+        
+        # Process blob data and organize it into sensor_data dictionary
+        global sensor_data
+        sensor_data = process_blob_data(blobs_data)
+
+        # Wait for 10 minutes before next update
+        time.sleep(600)
+
+# Start the background data update thread
+thread = threading.Thread(target=update_data)
+thread.daemon = True
+thread.start()
+
 @app.route('/')
-def list_blobs():
-    # Load configuration
-    blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
-    container_name = os.environ.get('AZURE_CONTAINER_NAME')
+def index():
+    return render_template("index.html")
 
-    # Print container name and connection string for debugging
-    print("Connection string is:", blob_connection_string)
-    print("Container name is:", container_name)
-    
-    # Check if environment variables are None
-    if not blob_connection_string:
-        return "Error: AZURE_BLOB_STORAGE_CONNECTION_STRING environment variable not set."
-    if not container_name:
-        return "Error: AZURE_CONTAINER_NAME environment variable not set."
-
-    # List and fetch blobs in the container
-    blob_data_list = list_and_fetch_blobs_in_container(blob_connection_string, container_name)
-    
-    # Process blob data and organize it into sensor_data dictionary
-    sensor_data = process_blob_data(blob_data_list)
-
-    # Check if sensor_data is None
-    if sensor_data is None:
-        return "Error occurred while processing blob data."
-
-    # Convert sensor_data to JSON string
-    sensor_data_json = json.dumps(sensor_data)
-
-    # Render the template with sensor data
-    return render_template("index.html", sensor_data_json=sensor_data_json)
+@app.route('/data')
+def get_data():
+    return jsonify(sensor_data)
 
 @app.route('/favicon.ico')
 def favicon():
