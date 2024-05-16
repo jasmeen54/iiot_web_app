6c3228c8066efb27f4c27e474a7cff65e0d19a05
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-16 09:41:13 +0200

    Update fetch_data.py

--- app/fetch_data.py
+++ app/fetch_data.py
@@ -1,36 +1,27 @@
 from azure.storage.blob import BlobServiceClient
-import os
+import json
 
-def list_blobs_in_container(connection_string, container_name):
+def list_blob_data(connection_string, container_name):
     try:
-        if not connection_string:
-            raise ValueError("Connection string is None.")
-        
-        print("Connecting to Blob Service...")
+        # Connect to the Blob Service
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        
+        # Get a reference to the container
         container_client = blob_service_client.get_container_client(container_name)
         
-        blobs_in_folders = {}
-        unique_prefixes = set()
-
-        # List all blobs and collect unique prefixes
+        # List blobs in the container
+        blob_data_list = []
         for blob in container_client.list_blobs():
-            # Assuming folder names end with a '/'
-            if '/' in blob.name:
-                prefix = blob.name.split('/')[0] + '/'
-                unique_prefixes.add(prefix)
+            if blob.name.endswith('.json'):
+                blob_client = container_client.get_blob_client(blob.name)
+                blob_data = blob_client.download_blob()
+                data = blob_data.readall().decode('utf-8')
+                blob_data_list.append(data)
         
-        # For each unique prefix, list blobs that end with .json
-        for prefix in unique_prefixes:
-            blobs_in_folders[prefix] = []
-            for blob in container_client.list_blobs(name_starts_with=prefix):
-                if blob.name.endswith('.json'):
-                    blobs_in_folders[prefix].append(blob.name)
-        
-        return blobs_in_folders
+        return blob_data_list
     
     except Exception as e:
-        print(f"Error listing blob names: {e}")
+        print(f"Error listing blob data: {e}")
         return None
 
 
