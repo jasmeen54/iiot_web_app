2b2600120f350891d5923a600202ce41f37b0945
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-16 15:33:49 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,34 +8,22 @@ import time
 
 app = Flask(__name__)
 
-# Initialize sensor_data as an empty dictionary
 sensor_data = {}
-sensor_data_lock = threading.Lock()  # Create a lock for synchronization
 
 def update_data():
     while True:
         blob_connection_string = os.environ.get('AZURE_BLOB_STORAGE_CONNECTION_STRING')
         container_name = os.environ.get('AZURE_CONTAINER_NAME')
 
-        print("Starting data update cycle...")
-        
         # List and fetch blobs in the container
         blobs_data = list_and_fetch_blobs_in_container(blob_connection_string, container_name)
-        print("Fetched blobs data:", blobs_data)
         
         # Process blob data and organize it into sensor_data dictionary
-        processed_data = process_blob_data(blobs_data)
-        print("Processed data:", processed_data)
-        
-        # Acquire the lock before updating sensor_data
-        with sensor_data_lock:
-            sensor_data.clear()  # Clear existing data
-            sensor_data.update(processed_data)  # Update with new data
+        global sensor_data
+        sensor_data = process_blob_data(blobs_data)
 
-        print("Updated sensor data:", sensor_data)
-
-        # Wait for 5 minutes before next update
-        time.sleep(300)
+        # Wait for 10 minutes before next update
+        time.sleep(600)
 
 # Start the background data update thread
 thread = threading.Thread(target=update_data)
@@ -44,21 +32,20 @@ thread.start()
 
 @app.route('/')
 def index():
-    return render_template("index.html", sensor_data=sensor_data)
+    return render_template("index.html")
 
 @app.route('/data')
 def get_data():
-    global sensor_data
-    # Acquire the lock before accessing sensor_data
-    with sensor_data_lock:
-        return jsonify(sensor_data)
+    return jsonify(sensor_data)
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
