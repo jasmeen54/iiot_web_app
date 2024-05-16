a3fd28110cdf036eeb51c14c75ef97760708b0b5
jasmeen54 <167179240+jasmeen54@users.noreply.github.com>
2024-05-16 14:19:39 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -21,12 +21,16 @@ def update_data():
         blobs_data = list_and_fetch_blobs_in_container(blob_connection_string, container_name)
         
         # Process blob data and organize it into sensor_data dictionary
-        processed_data = process_blob_data(blobs_data)
+        processed_data_array = process_blob_data(blobs_data)
 
         # Acquire the lock before updating sensor_data
         with sensor_data_lock:
-            sensor_data.clear()  # Clear existing data
-            sensor_data.update(processed_data)  # Update with new data
+            # Clear existing data
+            sensor_data.clear()
+            # Aggregate processed data into sensor_data dictionary
+            for processed_data in processed_data_array:
+                sensor_name = processed_data['sensor']
+                sensor_data[sensor_name] = processed_data
 
         # Wait for 5 minutes before next update
         time.sleep(300)
@@ -38,15 +42,14 @@ thread.start()
 
 @app.route('/')
 def index():
-    return render_template("index.html", sensor_data_json=sensor_data_json)
+    return render_template("index.html", sensor_data=sensor_data)
 
 @app.route('/data')
 def get_data():
     global sensor_data
     # Acquire the lock before accessing sensor_data
     with sensor_data_lock:
-        sensor_data_json = json.dumps(sensor_data)
-        return sensor_data_json
+        return jsonify(sensor_data)
 
 @app.route('/favicon.ico')
 def favicon():
